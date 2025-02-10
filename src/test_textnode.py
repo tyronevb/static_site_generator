833bf7560ff39244acd8c2d837ac267e30fa10c7
Tyrone van Balla <tyronevb@live.com>
2025-02-10 17:18:52 +0200

    add function to convert TextNodes to HTMLNodes with unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextType, TextNode
+from textnode import TextType, TextNode, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -28,3 +28,79 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
+    
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        text_node = TextNode(text="This is a normal text node",
+                         text_type=TextType.NORMAL_TEXT)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a normal text node")
+
+    def test_bold(self):
+        text_node = TextNode(text="This is a bold text node",
+                         text_type=TextType.BOLD_TEXT)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text node")
+
+    def test_image(self):
+        text_node = TextNode(text="This is an image node",
+                        text_type=TextType.IMAGES,
+                        url="www.dreaimages.test")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, 
+                         {"src": "www.dreaimages.test", "alt": "This is an image node"})
+    
+    def test_text_to_html_normal(self):
+        text_node = TextNode(text="This is a normal text node",
+                         text_type=TextType.NORMAL_TEXT)
+        
+        html = text_node_to_html_node(text_node)
+        self.assertEqual("LeafNode(None, This is a normal text node, None)", repr(html))
+
+    def test_text_to_html_normal_html(self):
+        text_node = TextNode(text="This is a normal text node",
+                         text_type=TextType.NORMAL_TEXT)
+        
+        html = text_node_to_html_node(text_node)
+        self.assertEqual("This is a normal text node", html.to_html())
+
+    def test_text_to_html_bold_html(self):
+        text_node = TextNode(text="This is a bold text node",
+                         text_type=TextType.BOLD_TEXT)
+        
+        html = text_node_to_html_node(text_node)
+        self.assertEqual("<b>This is a bold text node</b>", html.to_html())
+
+    def test_text_to_html_italics_html(self):
+        text_node = TextNode(text="This is an italics text node",
+                         text_type=TextType.ITALIC_TEXT)
+        
+        html = text_node_to_html_node(text_node)
+        self.assertEqual("<i>This is an italics text node</i>", html.to_html())
+
+    def test_text_to_html_code_html(self):
+        text_node = TextNode(text="This is a code node",
+                         text_type=TextType.CODE_TEXT)
+        
+        html = text_node_to_html_node(text_node)
+        self.assertEqual("<code>This is a code node</code>", html.to_html())
+    
+    def test_text_to_html_link_html(self):
+        text_node = TextNode(text="This is a link node",
+                         text_type=TextType.LINKS,
+                         url="www.drealinks.test")
+        
+        html = text_node_to_html_node(text_node)
+        self.assertEqual('<a href="www.drealinks.test">This is a link node</a>', html.to_html())
+
+    def test_text_to_html_image_html(self):
+        text_node = TextNode(text="This is an image node",
+                         text_type=TextType.IMAGES,
+                         url="www.dreaimages.test")
+        
+        html = text_node_to_html_node(text_node)
+        self.assertEqual('<img src="www.dreaimages.test" alt="This is an image node"></img>', html.to_html())
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+from htmlnode import LeafNode
 class TextType(Enum):
     NORMAL_TEXT = "text"
     BOLD_TEXT = "bold"
@@ -24,3 +24,21 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.NORMAL_TEXT:
+            return LeafNode(tag=None, value=text_node.text, props=None)
+        case TextType.BOLD_TEXT:
+            return LeafNode(tag="b", value=text_node.text, props=None)
+        case TextType.ITALIC_TEXT:
+            return LeafNode(tag="i", value=text_node.text, props=None)
+        case TextType.CODE_TEXT:
+            return LeafNode(tag="code", value=text_node.text, props=None)
+        case TextType.LINKS:
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        case TextType.IMAGES:
+            return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text,})
+        
+        case _: 
+            raise ValueError (f"Invalid Text Type: {text_node.text_type}")
