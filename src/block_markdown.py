509b9cefe7f886c7d6dc3c32f68553dbfda235c7
Tyrone van Balla <tyronevb@live.com>
2025-02-25 19:19:30 +0200

    add function to determine BlockType for a given block

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,54 @@
+from enum import Enum
+
+# this way of creating an Enum assignes the values to the variables starting form 1
+# BlockType = Enum("BlockType", ["PARAGRAPH", "HEADING", "CODE", "QUOTE", "UNORDERED_LIST", "ORDERED_LIST"])
+
+# this way of creating an Enum allows you to set the values to anything (is this correct?)
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quite"
+    OLIST = "ordered_list"
+    ULIST = "unordered_list"
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    # check for heading
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    # check for code
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    # check for quote
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    # check for * unordered list
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    # check for - unordered list
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    # check for ordered list
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
